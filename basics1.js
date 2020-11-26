92ed38f7f674737956d5c5812334f38331730c35
Mobin arshad khan <mobink09@gmail.com>
2020-11-26 14:04:51 +0530

    first commit

---
+++ basics1.js
@@ -0,0 +1,16 @@
+//  javaScript code tutorials
+// Class One ============= 1
+
+// alert(), prompt(), and confirm() only support in browser based application not in nodeJS (server based)
+
+////////////////////////////////////////////////////////////////////////////////////
+/*
+alert("This will be alerted in browser");
+let name = prompt("What is your name");
+let userInput = confirm("Are you really want to leave the page");
+if (userInput) {
+  alert("Thanks to stay with us");
+} else {
+  alert("Your welcome more");
+}
+*/
