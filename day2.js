b2143273a2a753d2778585ccc0bbac552ef98410
MobinArshad <74773257+MobinArshad@users.noreply.github.com>
2020-11-27 13:32:14 +0530

    variables naming

--- day2.js
+++ day2.js
@@ -1,2 +1,10 @@
 // javaScript variables
 // variables are defined three ways in java Script
+// var, let and const
+// var have function scope
+// let and const have block level scope
+// naming of variable letter, _  (underscore), $ and number , can't start with number
+let name = "Mobin Arshad Khan";
+var num = 112;
+const PI = 3.147;
+console.log(name, num, PI);
