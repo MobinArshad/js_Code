f0c373e17aecf9d89fab0543bd335f3441cc7911
MobinArshad <74773257+MobinArshad@users.noreply.github.com>
2020-11-30 12:07:19 +0530

    bmi calculator

--- day4.js
+++ day4.js
@@ -9,3 +9,44 @@ let gName = "xyz";
 console.log(
   `Mr ${bName} and your love is ${gName} and your chance of meeting of love is ${randomNumber}%`
 );
+
+// control statements if else if else and switch
+
+if (randomNumber >= 75) {
+  console.log(
+    "You love your love most and your love score is " + randomNumber + "%"
+  );
+} else {
+  console.log(
+    "You may love more than this... your love score is " + randomNumber + "%"
+  );
+}
+// calculating % and giving the grade to it
+const sbjMarks = {
+  subjects: ["Maths", "English", "Science", "Programming", "Designing"],
+  marks: [100, 85, 95, 90, 70],
+};
+
+let sum = 0;
+for (let i = 0; i < 5; i++) {
+  sum += sbjMarks.marks[i];
+  console.log(sbjMarks.subjects[i] + " " + sbjMarks.marks[i]);
+}
+let percentage = sum / 5;
+console.log(sum);
+console.log("Your Percentage is " + percentage + "%");
+
+function bmiCalculator(weight, height) {
+  let bmi = weight / Math.pow(height, 2);
+  let result = "";
+  if (bmi < 18.5) {
+    result = "Your BMI is " + bmi + ", so you are underweight.";
+  } else if (bmi >= 18.5 && bmi < 24.9) {
+    result = "Your BMI is " + bmi + ", so you have a normal weight.";
+  } else {
+    result = "Your BMI is " + bmi + ", so you are overweight.";
+  }
+  return result;
+}
+
+console.log(bmiCalculator(65, 1.8));
