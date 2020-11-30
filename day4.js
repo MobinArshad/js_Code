835a223ca5465ee358ac65b6e6c831d52776a1de
MobinArshad <74773257+MobinArshad@users.noreply.github.com>
2020-11-30 11:03:15 +0530

    day 4

---
+++ day4.js
@@ -0,0 +1,11 @@
+// Fourth day of javaScript
+// random Number generator
+
+let randomNumber = Math.round(Math.random() * 100);
+
+// love calculator just for fun
+let bName = "Mobin Arshad Khan";
+let gName = "xyz";
+console.log(
+  `Mr ${bName} and your love is ${gName} and your chance of meeting of love is ${randomNumber}%`
+);
