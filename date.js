5e1d721bab13212e1c079d598ba4407c9208353a
MobinArshad <74773257+MobinArshad@users.noreply.github.com>
2020-12-01 16:36:55 +0530

    changing DOM

---
+++ date.js
@@ -0,0 +1,14 @@
+// date and its objects
+//let date = new Date();
+// date = date.getTime();
+// date = date.getDate();
+//date = date.getFullYear();
+//console.log(date);
+
+// function to create data and time
+
+function getDateTime() {
+  let date = new Date();
+  return date;
+}
+console.log(getDateTime());
