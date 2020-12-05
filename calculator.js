a76f2e59975e4baf42b462937f18d9b5075d6b51
MobinArshad <74773257+MobinArshad@users.noreply.github.com>
2020-12-05 14:48:55 +0530

    adding calculator

---
+++ calculator.js
@@ -0,0 +1,31 @@
+// calculator
+// higher order functions => the function which take another function as input is called higher order functions
+
+// calculator()
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function reminder(num1, num2) {
+  return num1 % num2;
+}
+
+function calculator(num1, num2, operator) {
+  return operator(num1, num2);
+}
+
+let x = calculator(34, 7, reminder);
+console.log(x);
