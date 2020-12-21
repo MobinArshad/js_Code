d9686fd365645e417aa53f6b4ec21f56e63bd65a
MobinArshad <74773257+MobinArshad@users.noreply.github.com>
2020-12-21 11:28:14 +0530

    array & methods

--- day8_basics.js
+++ day8_basics.js
@@ -82,4 +82,80 @@ console.log(me[0]); // first character of the string
 console.log(me[1]); // second character of the string
 console.log(me[me.length - 1]); // last character of the string
 
-// Arrays
+// Store multiple values into arrays
+
+const sandwitch = ["peanut butter", "jelly", "bread", 0.8, true, "sugar"];
+console.log(sandwitch);
+
+// Nest one Array within Another Array => Multi-Dimensional array
+
+const items = [
+  ["bread", 5],
+  ["butter", "1kg"],
+  ["Apple", 100],
+  ["Mutton", "5kg"],
+];
+console.log(items);
+// access array with indexes
+const array = [50, 60, 70];
+console.log(array); // whole array
+console.log(array[0]);
+let data = array[1];
+console.log(data);
+// modify the array with indexes
+array[2] = 100;
+console.log(array);
+// access Multi-Dimensional Arrays with Indexes
+
+const arr = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [[10, 11, 12], 13, 14],
+];
+console.log(arr[3]);
+console.log(arr[3][0]);
+console.log(arr[3][0][1]);
+// push() method an easy way to append the data at the end of the array
+var arr1 = [1, 2, 3, 4];
+arr1.push(5);
+console.log(arr1);
+
+var arr2 = ["Stimpson", "J", "cat"];
+arr2.push(["Happy", "joy"]);
+console.log(arr2);
+
+// Manipulating arrays with pop() method
+let threeArr = [1, 4, 6];
+console.log(threeArr);
+let oneDown = threeArr.pop();
+console.log(oneDown);
+console.log(threeArr);
+
+// shift() are used to delete elements from the begining of the array
+let ourArray = ["Stipson", "J", ["cat"]];
+var removedFromArray = ourArray.shift();
+console.log(removedFromArray);
+console.log(ourArray);
+
+// unshift() add items or elements at the begining of the array
+ourArray.unshift("Happy");
+console.log(ourArray);
+// shoping list
+
+const list = [
+  ["chocolate", 50],
+  ["bread", 20],
+  ["milk", 2],
+  ["sugar", 1],
+  ["butter", 3],
+];
+
+for (let i = 0; i < list.length; i++) {
+  for (let j = 0; j < list[i].length; j++) {
+    if (typeof list[i][j] === "number") {
+    } else {
+      console.log("You have to buy" + list[i][j]);
+    }
+  }
+}
