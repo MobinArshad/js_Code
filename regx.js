392730957c864f6e8cbdf0ec4210896ec2841bac
MobinArshad <74773257+MobinArshad@users.noreply.github.com>
2020-12-30 18:39:58 +0530

    third commit in rgx

--- regx.js
+++ regx.js
@@ -64,3 +64,46 @@ str = "Let's have fun with regular expressions!";
 regex = /.un/;
 x = regex.test(str);
 console.log("Wild Card  : " + x);
+//Match Single Character with Multiple Possibilities
+
+let bigStr = "big";
+let bagStr = "bag";
+let bugStr = "bug";
+let bogStr = "bog";
+let bgRegex = /b[aiu]g/;
+console.log(bigStr.match(bgRegex));
+console.log(bagStr.match(bgRegex));
+console.log(bugStr.match(bgRegex));
+console.log(bogStr.match(bgRegex));
+// Match Letters of the Alphabet
+
+let catStr = "cat";
+let batStr = "bat";
+let matStr = "mat";
+let regex1 = /[a-e]at/;
+console.log(catStr.match(regex1));
+console.log(batStr.match(regex1));
+console.log(matStr.match(regex1));
+
+// Match Numbers and Letters of the Alphabet
+let jennyStr = "Jenny8675309";
+let myRegex = /[a-z0-9]/gi;
+console.log(jennyStr.match(myRegex));
+
+//Match Single Characters Not SpecifiedPassed
+// negated character set.
+// caret ^
+// e.g /[^aeiou]/gi => Matches all the character that are not vowels
+
+//Create a single regex that matches all characters that are not a number or a vowel. Remember to include the appropriate flags in the regex.
+let quoteSample = "3 blind mice.";
+let rgx = /[^aeiou0-9]/gi;
+console.log(quoteSample.match(rgx));
+
+//Match Characters that Occur One or More TimesPassed
+
+// You want to find matches when the letter s occurs one or more times in "Mississippi". Write a regex that uses the + sign.
+
+let difficultSpelling = "Mississippi";
+let rgx1 = /s+/gi;
+console.log(difficultSpelling.match(rgx1));
