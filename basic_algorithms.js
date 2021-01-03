89f0896513f4265d2ec41f72fc6b7d6ba4628352
MobinArshad <74773257+MobinArshad@users.noreply.github.com>
2021-01-03 17:55:17 +0530

    bool who

---
+++ basic_algorithms.js
@@ -0,0 +1,16 @@
+// basic js alogorithms continue
+
+// check data is boolean or not
+
+function checkBool(bool){
+    if(typeof bool === "boolean"){
+        return true;
+    } else{
+        return false;
+    }
+}
+
+let x = checkBool(true);
+console.log(x);
+let y = checkBool(false);
+console.log(y);
